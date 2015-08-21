a32a064f29cb65ed8350dcbca730f8cdd4fab0bf
Are Almaas <mralmaas@gmail.com>
2015-08-21 10:51:22 +0200

    indent and minification

--- ng-segmentio.js
+++ ng-segmentio.js
@@ -1,8 +1,8 @@
 'use strict';
 
-angular.module('segmentio', ['ng'])
+segmentio.$inject = ['$rootScope', '$window', '$location', '$log', '$q'];
 
-.factory('segmentio', function($rootScope, $window, $location, $log, $q) {
+function segmentio($rootScope, $window, $location, $log, $q) {
   var service = {};
 
   $window.analytics = $window.analytics || [];
@@ -10,8 +10,8 @@ angular.module('segmentio', ['ng'])
   // Define a factory that generates wrapper methods to push arrays of
   // arguments onto our `analytics` queue, where the first element of the arrays
   // is always the name of the analytics.js method itself (eg. `track`).
-  var methodFactory = function(method) {
-    return function() {
+  var methodFactory = function (method) {
+    return function () {
       var args = Array.prototype.slice.call(arguments);
       args.unshift(method);
       $window.analytics.push(args);
@@ -32,9 +32,9 @@ angular.module('segmentio', ['ng'])
   /**
    * @description
    * Load Segment.io analytics script
-   * @param apiKey The key API to use
+   * @param key The key API to use
    */
-  service.load = function(key) {
+  service.load = function (key) {
     var deferred = $q.defer();
 
     if (document.getElementById('analytics-js')) {
@@ -63,11 +63,14 @@ angular.module('segmentio', ['ng'])
   $window.analytics.SNIPPET_VERSION = '3.0.1';
 
   // Listening to $viewContentLoaded event to track pageview
-  $rootScope.$watch(function() {
+  $rootScope.$watch(function () {
     return $location.path();
-  }, function(value) {
+  }, function (value) {
     service.page(value);
   });
 
   return service;
-});
+}
+
+angular.module('segmentio', ['ng'])
+  .factory('segmentio', segmentio);
